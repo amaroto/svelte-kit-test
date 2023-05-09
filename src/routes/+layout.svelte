ee8e575bda5bc9651cae90f754b0bbfb2a1c0126
Alejandro Maroto <SESA719724@se.com>
2023-05-09 16:05:15 +0200

    first commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Footer from './footer.svelte';
+	import Header from './header.svelte';
+</script>
+
+<Header />
+
+<slot />
+
+<Footer />
---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,3 @@
+<h2>Contact</h2>
+
+<a href="/">Homepage</a>
